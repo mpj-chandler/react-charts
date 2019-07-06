e0c328a3f68cde198f056a7351566bb8195aea88
marcus.chandler <marcus.chandler@gfk.com>
2019-07-06 19:37:41 +0100

    Added SASS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import styles from './App.scss';
 
 interface AppProps {
 
 }
 
-const App: React.FC<AppProps> = (props) => <div {...props}>React Charts</div>;
+const App: React.FC<AppProps> = (props) => {
+    return (
+        <div className={styles.App} {...props}>
+            <span className={styles.App__Title}>React Charts</span>
+            <div className={styles.App__ChartContainer}>ChartGoesHere</div>
+        </div>
+    );
+};
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const postcssPresetEnv = require('postcss-preset-env');
 
 module.exports = {
     // Change to your "entry-point".
@@ -13,12 +14,79 @@ module.exports = {
         extensions: ['.ts', '.tsx', '.js', '.json']
     },
     module: {
-        rules: [{
-            // Include ts, tsx, js, and jsx files.
-            test: /\.(ts|js)x?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        }],
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.(sass|scss)$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[local]'
+                            },
+                            localsConvention: 'asIs',
+                            importLoaders: 2,
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [
+                                postcssPresetEnv({
+                                    autoprefixer: {grid: true}
+                                })
+                            ],
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|svg|gif|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    name: 'assets/[name].[hash].[ext]',
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[local]'
+                            },
+                            localsConvention: 'asIs',
+                            importLoaders: 2,
+                            sourceMap: true
+                        }
+                    }
+                ]
+            },
+            ],
     },
     devServer: {
         compress: true,
