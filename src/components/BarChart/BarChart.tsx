fe3b7299b0f8c57010d655ce6185d03829b64991
marcus.chandler <marcus.chandler@gfk.com>
2019-07-07 21:36:02 +0100

    Added XAxis with ticks

--- src/components/BarChart/BarChart.tsx
+++ src/components/BarChart/BarChart.tsx
@@ -18,7 +18,7 @@ const BarChart: React.FC<BarChartProps> = (props) => {
             <div className={styles.BarChart__title}>{props.title}</div>
             <div className={styles.BarChart__yAxis}>yAxis</div>
             <div className={styles.BarChart__blank}>()</div>
-            <XAxis range={{ min: 0, max: 100 }}/>
+            <XAxis numTicks={10} range={{ min: 0, max: 100 }}/>
         </div>
     )
 }
--- src/components/XAxis/XAxis.tsx
+++ src/components/XAxis/XAxis.tsx
@@ -4,11 +4,33 @@ import styles from './XAxis.scss';
 
 interface XAxisProps {
     range: AxisRange;
+    numTicks: number;
 }
 
 const XAxis: React.FC<XAxisProps> = (props) => {
+    const renderTicks: (props: XAxisProps) => JSX.Element = (props) => {
+        const tickLabels: string[] = [];
+        const interval = props.range.max - props.range.min;
+        const increment = 0.9 * (interval / props.numTicks);
+
+        for (let i=0; i<=props.numTicks; i++) {
+            tickLabels.push(`${props.range.min + (i * increment)}`)
+        };
+
+        return (
+            <g className={styles.XAxis__Ticks}>
+                {tickLabels.map((label: string, index: number) => <line x1={`${5 + (index * increment)}%`} y1={'50%'} x2={`${5 + (index * increment)}%`} y2={'60%'} stroke={'black'}/>)}
+            </g>
+        )
+    }
+
     return (
-        <div className={styles.XAxis}>xAxis</div>
+        <div className={styles.XAxis}>
+            <svg className={styles.XAxis__Svg}>
+                <line x1={'5%'} y1={'50%'} x2={'95%'} y2={'50%'} stroke={'black'} fill={'transparent'} strokeWidth={1}/>
+                {renderTicks(props)}
+            </svg>
+        </div>
     )
 };
 
